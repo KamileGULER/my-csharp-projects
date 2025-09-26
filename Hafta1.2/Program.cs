b8e39baa10349031f2e2d1ba7aadaa45095fe570
Kamile GÜLER <kamile.guler32@gmail.com>
2025-09-26 10:18:29 +0300

    Initial commit: C# project

---
+++ Hafta1.2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hafta1._2
+{
+    class Bread
+    {
+        public int counter;
+    }
+
+    static class Butter
+    {
+        public static int counter;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instance of class "Butter" required
+            Bread a = new Bread();
+            a.counter = 5;
+            Console.WriteLine("Bread: " + a.counter);
+
+            //no instance required
+            Butter.counter = 7;
+            Console.WriteLine("Butter:" + Butter.counter);
+        }
+    }
+}
